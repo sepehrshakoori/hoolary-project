90eb711be07fb1ba0e81cd91329f0184d170ad62
Sepehr Shakoori <S.shakoori@persianswitch.net>
2023-11-13 08:53:06 +0330

    learn Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClockComponent } from './clock/clock.component';
+import { CalanderComponent } from './calander/calander.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClockComponent,
+    CalanderComponent
   ],
   imports: [
     BrowserModule,
